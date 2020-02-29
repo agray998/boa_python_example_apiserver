c924e9ffa424c358adae6d61ecc2c4f7c61ff331
bob-crutchley <bob.crutchley96@gmail.com>
2020-02-29 10:36:01 +0000

    return response object now

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
 @app.route('/get/text', methods=['GET'])
 def get_text():
-    return 'Hello from Flask'
+    return Response('Hello from Flask', mimetype='text/plain')
 
 @app.route('/post/text', methods=['POST'])
 def post_text():
-    return "Data you sent: " + request.data.decode("utf-8")
+    return Response("Data you sent: " + request.data.decode("utf-8"), mimetype='text/plain')
 
 @app.route('/get/json', methods=['GET'])
 def get_json():
-    return {"data": "Hello from Flask"}
+    return Response({"data": "Hello from Flask"}, mimetype='application/json')
 
 @app.route('/post/json', methods=['POST'])
 def post_json():
-    return {"data": request.data}
+    return Response({"data": request.get_json()}, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
